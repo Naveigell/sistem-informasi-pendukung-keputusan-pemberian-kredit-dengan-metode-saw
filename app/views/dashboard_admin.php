3d8ed1f8b1709090091c0e4499f9d4c72f083caf
ratihpurwaning <purwaningratih@gmail.com>
2023-04-07 12:10:58 +0800

    change dashboard admin page

--- app/views/dashboard_admin.php
+++ app/views/dashboard_admin.php
@@ -11,6 +11,17 @@
         <div class="header-body">
             <div class="row align-items-center py-4">
                 <div class="col-lg-6 col-7">
+                    <h6 class="h2 text-white d-inline-block mb-0 ">Dashboards</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- <div class="header bg-primary pb-6">
+    <div class="container-fluid">
+        <div class="header-body">
+            <div class="row align-items-center py-4">
+                <div class="col-lg-9 col-4">
                     <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                         <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
                             <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
@@ -18,25 +29,25 @@
                         </ol>
                     </nav>
                 </div>
-            </div>
+            </div> -->
             <!-- Card stats -->
-            <div class="row">
+            <!-- <div class="row">
                 <div class="col-xl-4 col-md-6">
                     <div class="card card-stats">
-                        <!-- Card body -->
-                        <div class="card-body">
+                       Card body -->
+                        <!-- <div class="card-body">
                             <div class="row">
                                 <div class="col">
                                     <h5 class="card-title text-uppercase text-muted mb-0">Total kriteria</h5>
-                                    <span class="h2 font-weight-bold mb-0"><?= $kriteria; ?></span>
-                                </div>
-                                <div class="col-auto">
+                                    <span class="h2 font-weight-bold mb-0"><?= $kriteria; ?></span> -->
+                                <!-- </div> -->
+                                <!-- <div class="col-auto">
                                     <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
                                         <i class="ni ni-collection"></i>
                                     </div>
                                 </div>
-                            </div>
-                            <p class="mt-3 mb-0 text-sm">
+                            </div> -->
+                            <!-- <p class="mt-3 mb-0 text-sm">
                                 <a href="<?= BASE_PATH; ?>/criteria" class="text-nowrap text text-dark">Lihat kriteria</a>
                             </p>
                         </div>
@@ -45,17 +56,17 @@
                 <div class="col-xl-4 col-md-6">
                     <div class="card card-stats">
                         <!-- Card body -->
-                        <div class="card-body">
+                        <!-- <div class="card-body">
                             <div class="row">
                                 <div class="col">
                                     <h5 class="card-title text-uppercase text-muted mb-0">Total sub kriteria</h5>
                                     <span class="h2 font-weight-bold mb-0"><?= $sub_kriteria; ?></span>
-                                </div>
-                                <div class="col-auto">
+                                </div> -->
+                                <!-- <div class="col-auto">
                                     <div class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
                                         <i class="ni ni-ungroup"></i>
-                                    </div>
-                                </div>
+                                    </div> -->
+                                <!-- </div>
                             </div>
                             <p class="mt-3 mb-0 text-sm">
                                 <a href="<?= BASE_PATH; ?>/criteria" class="text-nowrap text text-dark">Lihat sub kriteria</a>
@@ -66,17 +77,17 @@
                 <div class="col-xl-4 col-md-6">
                     <div class="card card-stats">
                         <!-- Card body -->
-                        <div class="card-body">
+                        <!-- <div class="card-body">
                             <div class="row">
                                 <div class="col">
                                     <h5 class="card-title text-uppercase text-muted mb-0">Total nasabah</h5>
                                     <span class="h2 font-weight-bold mb-0"><?= $nasabah; ?></span>
-                                </div>
-                                <div class="col-auto">
+                                </div> --> 
+                                <!-- <div class="col-auto">
                                     <div class="icon icon-shape bg-gradient-green text-white rounded-circle shadow">
                                         <i class="ni ni-circle-08"></i>
-                                    </div>
-                                </div>
+                                    </div> -->
+                                <!-- </div>
                             </div>
                             <p class="mt-3 mb-0 text-sm">
                                 <a href="<?= BASE_PATH; ?>/nasabah" class="text-nowrap text text-dark">Lihat nasabah</a>
@@ -87,7 +98,60 @@
             </div>
         </div>
     </div>
-</div>
+</div> --> 
+<div class="container-fluid mt--6">
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="mb-0"style="text-align:center;">LPD DESA BURUAN</h1>
+                    <!-- <div class="text text-center">
+                        <img src="<?= BASE_PATH; ?>/public/assets/img/lpd.jpeg" alt="..." style="width:50%; height:50%; display:inline-block; text-align:center;">
+                    </div>  -->
+                    <div class="row mt-3">
+                    <div class="col-6">
+                            <img src="<?= BASE_PATH; ?>/public/assets/img/lpd.jpeg" alt="..." style="width:100%; height:100%; display:inline-block; text-align:center;">
+                        </div>
+                        <div class="col-6">
+                            <p class="text-md mb-0" style="text-align:justify;">
+                            LPD Desa Pakraman Buruan didirikan pada tanggal 29 September 1986 berdasarkan Surat Keputusan (SK) Gubernur Kepala Daerah (Perda) Provinsi Bali No. 272 Tahun 1986. LPD Desa Pakraman Buruan beralamat di Jl. Batukaru No.63-71, Buruan, Penebel, Kabupaten Tabanan dan dipimpin oleh I Ketut Wirayasa sebagai kepala LPD. Hingga saat ini jumlah nasabah sebanyak 1967 orang, hingga saat ini jumlah nasabah sebanyak 10 orang yang mengajukan kredit tiap bulannya.
+                         
+                            </p>
+                        </div>
+                        
+                    </div>      
+
+                    <h2 class="mt-3">Visi</h2>
+                    <ol>
+                        <li>Menjadikan LPD Buruan sebagai lembaga keuangan yang sehat, 
+                            kuat, produktif dan berkembang secara wajar, 
+                            yang dipercaya krama desa.</li>
+                    </ol>
+                    <h2>Misi</h2>
+                    <ol style="text-align: justify;">
+                        <li>Meningkatkan Keimanan dalam mengurus dan mengelola LPD melalui 
+                            pemahaman ajaran Agama dan Etika kerja sehingga terwujud prajuru dan karyawan LPD dan panureksa 
+                            LPD yang profesional berdasarkan pengabdian tulus ikhlas untuk tetap lestarinya Desa Adat.</li>
+                        <li>Meningkatkan perekonomian krama dengan mendorong pertumbuhan usaha mikro, 
+                            kecil dan menengah dengan menunjang pembangunan desa adat.</li>
+                        <li>Meningkatkan dan mendorong pertumbuhan perekonomian dan pembangunan 
+                            desa adat Buruan serta sebagai sumber pendapatan desa.</li>
+                        <li>Meningkatkan kinerja, mempertahankan dan mengembangkan LPD melalui sistem pengelolaan dan pelayanan prima dengan 
+                            melaksanakan standar kerja Organisasi dan manajemen SDM LPD serta meningkatkan kwalitas SDM LPD.</li>
+                        <li>Meningkatkan daya saing melalui inovasi produk dan peningkatan efisiensi 
+                            untuk dapat menyediakan jasa pelayanan yang berkualitas dan harga yang kompetetive.</li>
+                        <li>Meningkatkan kepedulian LPD terhadap lingkungan desa terutama kepentingan sosial, budaya dan agama.</li>
+                        <li>Mewujudkan pemerataan kesepakatan berusaha dan peluang kerja bagi krama desa adat.</li>
+                    </ol>
+                    
+                </div>
+                <div class="card-body">
+                    <div class="col-md-12">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <div class="container-fluid mt--6">
     <div class="row">
         <div class="col-xl-12">
